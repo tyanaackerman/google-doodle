7477725908ad1b333541b6faf2e93da172ffe615
tyanaackerman <60484066+tyanaackerman@users.noreply.github.com>
2021-03-15 20:56:52 -0700

    Added html, css, & js

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,15 @@ gsap.from("p", {
   x: 200,
   y: 200
 });
+
+gsap.from("#snow", {
+  y: -200,
+  duration: 2,
+  repeat: -1
+});
+
+gsap.from("#snow1", {
+  y: -400,
+  duration: 2,
+  repeat: -1
+});
